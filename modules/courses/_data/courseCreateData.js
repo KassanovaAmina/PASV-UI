afe4578f3abeb35294138467742615056d799607
Viktor Bogutskii <info@siteograf.com>
2020-02-29 15:54:23 -0800

    prepare

--- modules/courses/_data/courseCreateData.js
+++
@@ -1,9 +0,0 @@
-const courseRandom999 = Math.trunc(Math.random() * 1000);
-
-const courseData = {
-  name: courseRandom999 + ' Course for adding Challenge',
-  description: 'description, description',
-  type: 'all',
-};
-
-export { courseData };
--- modules/courses/_data/courseStart.data.js
+++
@@ -1,9 +0,0 @@
-const courseName = 'PASV-UI TEST' + Math.trunc(Math.random() * 100);
-
-const newCourseCreate = {
-  courseName: courseName,
-  description: 'Test Test Test Test Test',
-  accessType: 'All',
-};
-
-export { newCourseCreate };
